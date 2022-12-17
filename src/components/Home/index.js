8a0f3739e8ce1f1f58a5df1c44dfffa2d1e4e11c
Merick <szetomerick@gmail.com>
2022-12-17 00:16:32 -0800

    initial commit barebones

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,52 @@
 import LogoTitle from '../../assets/images/logo-s.png';
 import {Link} from 'react-router-dom'
 import './index.scss';
-
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+import Logo from './Logo'
+import Loader from 'react-loaders';
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['z','e','t','o',',  ','M','e','r','i','c','k']
+    const jobArray = ['w','e','b',' ', 'd','e','v','e','l','o','p','e','r','.']
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, []);
+
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
-                <img src={LogoTitle} alt="developer"/>
-                Merick
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span>
                 <br />
-                web developer
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+
+
+                <img src={LogoTitle} alt="developer"/>
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}/>
+                <br />
+
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}/>
                 </h1>
                 <h2> Fullstack Developer / Python / Javascript</h2>
                 <Link to="contact" className='flat-button'>CONTACT ME </Link>
                 </div>
+                <Logo/>
 
         </div>
-    );
+        <Loader type="pacman" />
+        </>
+    )
 }
 
 export default Home
