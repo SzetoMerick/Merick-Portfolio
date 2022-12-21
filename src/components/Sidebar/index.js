c4b4c6485e598e662e53ec6d96efd206ac69eaf1
Merick <szetomerick@gmail.com>
2022-12-20 21:24:08 -0800

    not yet optimized

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,67 +1,112 @@
-import './index.scss';
-import { Link, NavLink } from 'react-router-dom';
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons';
-import { faGitlab, faLinkedin, faYoutube, faReadme, } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelope,
+  faHome,
+  faUser,
+  faBars,
+  faClose
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faGitlab,
+  faLinkedin,
+  faYoutube,
+  faReadme,
+} from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
 
-
-const Sidebar = () =>
+  return (
     <div className="nav-bar">
-        <Link className = "logo" to='/'>
-            <img src={LogoS} alt = "logo"/>
-            <img className ="sub-logo" src={LogoSubtitle} alt = "szeto"/>
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="szeto" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" to="/">
 
-        </Link>
-        <nav>
-            <NavLink exact = "true" activeclassname = "active"  to="/">
-                <FontAwesomeIcon icon = {faHome} color = "#4d4d4e"/>
-            </NavLink>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
 
-            <NavLink exact = "true" activeclassname = "active" className= "about-link" to="/about">
-                <FontAwesomeIcon icon = {faUser} color = "#4d4d4e"/>
-            </NavLink>
+        <NavLink
+        onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
 
-            <NavLink exact = "true" activeclassname = "active" className= "contact-link" to="/contact">
-                <FontAwesomeIcon icon = {faEnvelope} color = "#4d4d4e"/>
-            </NavLink>
-            <NavLink exact = "true" activeclassname = "active" className= "portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon = {faReadme} color = "#4d4d4e"/>
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a
-                target = "_blank"
-                rel="noreferrer"
-                href="https://www.linkedin.com/in/merick-szeto-89ba53195/"
-                >
-                    <FontAwesomeIcon icon = {faLinkedin} color = "#4d4d4e"/>
-                </a>
-            </li>
-            <li>
-                <a
-                target = "_blank"
-                rel="noreferrer"
-                href="https://www.linkedin.com/in/merick-szeto-89ba53195/"
-                >
-                    <FontAwesomeIcon icon = {faGitlab} color = "#4d4d4e"/>
-                </a>
-            </li>
-            <li>
-                <a
-                target = "_blank"
-                rel="noreferrer"
-                href="https://www.linkedin.com/in/merick-szeto-89ba53195/"
-                >
-                    <FontAwesomeIcon icon = {faYoutube} color = "#4d4d4e"/>
-                </a>
-            </li>
-        </ul>
-
+        <NavLink
+        onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+        onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faReadme} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/merick-szeto-89ba53195/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/merick-szeto-89ba53195/"
+          >
+            <FontAwesomeIcon icon={faGitlab} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/merick-szeto-89ba53195/"
+          >
+            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
     </div>
+  )
+}
 
-
-export default Sidebar;
+export default Sidebar
